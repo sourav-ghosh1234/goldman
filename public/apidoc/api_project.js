7c2cfe29330eb279c218328580082326f6162d85
Indrasrk555 <indrasroy543@gmail.com>
2021-09-08 16:43:52 +0530

    update

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -11,8 +11,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-07T15:45:31.712Z",
+    "time": "2021-09-08T10:52:28.764Z",
     "url": "https://apidocjs.com",
-    "version": "0.28.1"
+    "version": "0.29.0"
   }
 });
